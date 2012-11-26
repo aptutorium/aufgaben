e9fd57d5a459596dcd7bc4d6ae5ba2efc80d7c74
Nils Becker <info@nils-becker.com>
2012-11-26 21:11:05 +0100

    added array example

---
+++ Termin 7/einlesen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main( void )
+{
+
+  const int size = 5;
+  int i;
+  int input = 0;
+  int values[5];
+
+  // einlesen der zahlen
+  for (i = 0; i < size; ++i)
+  {
+    printf("Bitte geben Sie eine Zahl ein: ");
+    scanf("%d", &input);
+    values[i] = input;
+  }
+
+  // ausgabe der zahlen
+  printf("\nFolgende Zahlen wurden eingegeben:");
+  printf("\n----------------------------------\n");
+  for (i = 0; i < size; ++i)
+  {
+    printf("%d. Zahl: %d\n", i+1, values[i]);
+  }
+  printf("----------------------------------\n");
+
+  return 0;
+}
